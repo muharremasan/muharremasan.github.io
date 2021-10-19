96898aa4ebb0adffb0bfa7b6848c22389aefa796
muharremasan <66525003+muharremasan@users.noreply.github.com>
2021-10-19 16:32:23 +0200

    Update

--- cog1/A2/modelData/myModel.js
+++ cog1/A2/modelData/myModel.js
@@ -17,19 +17,10 @@ define(["exports", "data"], function(exports, data) {
 		
 		if(parameter) {
 			var scale = parameter.scale;
-			var textureURL = parameter.textureURL;
-			// Each face shows a different area of the given texture (e.g, a dice).
-			var sixFacesTexture = parameter.sixFacesTexture;
 		}
 		// Set default values if parameter is undefined.
 		if(scale == undefined){
 			scale = 200;
-		}
-		if(textureURL == undefined){
-			textureURL = "";
-		}
-		if(sixFacesTexture == undefined){
-			sixFacesTexture = false;
 		}
 
 		// Instance of the model to be returned.
@@ -61,41 +52,6 @@ define(["exports", "data"], function(exports, data) {
 			[4,7,7]
 		];	
 		instance.polygonColors = [2,8,2,8,2,8,2,8,2,8];
-		
-
-		if( ! sixFacesTexture){
-	        // Use default texture coordinates.
-			// instance.textureCoord = [];	
-			// For order of corners of faces, see polygonVertices.
-			instance.polygonTextureCoord = [
-				[1,2,3,0],
-				[1,2,3,0],
-				[1,0,3,2],
-				[3,0,1,2],
-				[3,0,1,2],
-				[3,0,1,2]
-			];
-		} else {
-			// BEGIN exercise Cube-Dice-Texture
-			
-			// Order 0 to 16 form bottom-left to top-right
-			// line by line, indices in spatial order:
-			instance.textureCoord = [];
-			// ...
-
-			// Use textureCoord in order given for textureCoord.
-			// The order of corners of faces must fit the one given in polygonVertices.
-			// Match orientation of face given for polygonVertices.
-			// D=bottom/down, U=top/up, F=front, R=right, B=back, L=left
-			// The mapping is explained on the texture image.
-			// instance.polygonTextureCoord = [ ....];
-
-			// END exercise Cube-Dice-Texture			
-		}
-		
-		instance.textureURL = textureURL;
-
-		data.applyScale.call(instance, scale);
 
 		return instance;		
 	};
