17d5b65dc0034204cb8f187c45dbb00e3393db4e
muharremasan <66525003+muharremasan@users.noreply.github.com>
2021-10-20 21:03:24 +0200

    Update

--- cog1/A2/modelData/myModel.js
+++ cog1/A2/modelData/myModel.js
@@ -16,35 +16,16 @@
 	 * @parameter scale is the edge length of the cube.
 	 * @returns instance of this model.
 	 */
-	exports.create = function(parameter) {
-		
+	 exports.create = function(parameter) {
 		if(parameter) {
 			var scale = parameter.scale;
-			var textureURL = parameter.textureURL;
-			// Each face shows a different area of the given texture (e.g, a dice).
-			var sixFacesTexture = parameter.sixFacesTexture;
 		}
 		// Set default values if parameter is undefined.
 		if(scale == undefined){
 			scale = 200;
 		}
-		if(textureURL == undefined){
-			textureURL = "";
-		}
-		if(sixFacesTexture == undefined){
-			sixFacesTexture = false;
-		}
 
-		// Instance of the model to be returned.
 		var instance = {};
-
-		// Vertex indices:							
-		//   7----6
-		//	/|   /|
-		// 4----5 |
-		// | 3--|-2
-		// |/   |/
-		// 0----1
 		instance.vertices = [
 			[-0.2,-0.2, 0.2], //0
 			[ 0.05,-0.2, 0.2], //1
@@ -66,11 +47,7 @@
 			[ 0.05,0.2,-0.2], //14
 			[0.2,0.2,-0.2], //15
 		];
-		// Use default colors, implicitly.
-		// instance.colors = data.colors;
 
-		// Corners of the faces have to fit the texture coordinates.			
-		// Faces: bottom/down, top/up, front, right, back, left. 
 		instance.polygonVertices = [
 			[3,2,1,0],
 			[4,5,6,7],
@@ -88,40 +65,7 @@
 		];	
 
 		instance.polygonColors = [2,8,2,8,2,8,2,8,2,8,2,8];
-		
-		//instance.vertexNormals = [];
-		//instance.polygonNormals = [];
 
-		if( ! sixFacesTexture){
-	        // Use default texture coordinates.
-			// instance.textureCoord = [];	
-			// For order of corners of faces, see polygonVertices.
-			instance.polygonTextureCoord = [
-				[1,2,3,0],
-				[1,2,3,0],
-				[1,0,3,2],
-				[3,0,1,2],
-				[3,0,1,2],
-				[3,0,1,2]
-			];
-		} else {
-			// BEGIN exercise Cube-Dice-Texture
-			
-			// Order 0 to 16 form bottom-left to top-right
-			// line by line, indices in spatial order:
-			instance.textureCoord = [];
-			// ...
-
-			// Use textureCoord in order given for textureCoord.
-			// The order of corners of faces must fit the one given in polygonVertices.
-			// Match orientation of face given for polygonVertices.
-			// D=bottom/down, U=top/up, F=front, R=right, B=back, L=left
-			// The mapping is explained on the texture image.
-			// instance.polygonTextureCoord = [ ....];
-
-			// END exercise Cube-Dice-Texture			
-		}
-		
 		instance.textureURL = textureURL;
 
 		data.applyScale.call(instance, scale);
