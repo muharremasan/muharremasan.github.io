cb9f43315fed7c1feef9b04af9530b372922051c
muharremasan <66525003+muharremasan@users.noreply.github.com>
2021-12-12 22:50:33 +0100

    Add files via upload

--- cog1/A5/raster.js
+++ cog1/A5/raster.js
@@ -646,20 +646,20 @@ function(exports, shader, framebuffer, data) {
 		// The result is the distance D of polygon plane to origin.
 
 		// // Check result, applying the plane equation to the original polygon vertices.
-		// for(var i = 0; i < polygon.length; i++) {
-		//     var p = polygon[i];
-		//     var x = vertices[p][0];
-		//     var y = vertices[p][1];
-		//     var z = vertices[p][2];
-		//     var zCalc = getZ(x, y);
+		for(var i = 0; i < polygon.length; i++) {
+		    var p = polygon[i];
+		    var x = vertices[p][0];
+		    var y = vertices[p][1];
+		    var z = vertices[p][2];
+		    var zCalc = getZ(x, y);
             
-		// 	D = -(A * x + B * y + C * z);
+			D = -(A * x + B * y + C * z);
 
-		// if(Math.abs(z - zCalc) > 0.001) {
-		//     console.log("Check failed  z "+z+" = "+zCalc);
-		//     console.log("Plane: A=" + A + " B=" + B + " C=" + C + " D=" + D);
-		//  }
-		// }
+		if(Math.abs(z - zCalc) > 0.001) {
+		    console.log("Check failed  z "+z+" = "+zCalc);
+		    console.log("Plane: A=" + A + " B=" + B + " C=" + C + " D=" + D);
+		 }
+		}
 
 		// END exercise Z-Buffer
 
